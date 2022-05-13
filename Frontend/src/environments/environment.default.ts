04f8d4e92b0768a57b5e866cdebb50a88cedcc75
Rebecca Wölfle <rebecca.wolfle@tietoevry.com>
2022-05-13 13:04:03 +0200

    initial commit

---
+++ Frontend/src/environments/environment.default.ts
@@ -0,0 +1,7 @@
+// these are the default config used in every environment based on test usage
+// any key can be overwritten with the spread operator
+export const environment = {
+  version: require('../../package.json').version,
+  production: false,
+  useMockService: false
+};
---
+++ Frontend/src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+import { environment as defaultEnvironment } from './environment.default';
+
+export const environment = {
+  ...defaultEnvironment,
+  production: true
+};
